c0a580be3dea86573cf1345261e9db264b3a7f31
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2022-12-12 20:47:18 +0000

    update on backend and frontend

--- app/reservations/mutations/deleteReservation.ts
+++ app/reservations/mutations/deleteReservation.ts
@@ -3,7 +3,7 @@ import db from "db"
 import { z } from "zod"
 
 const DeleteReservation = z.object({
-  id: z.number(),
+  id: z.string(),
 })
 
 export default resolver.pipe(
--- app/reservations/mutations/updateReservation.ts
+++ app/reservations/mutations/updateReservation.ts
@@ -2,18 +2,23 @@ import { resolver } from "@blitzjs/rpc"
 import db from "db"
 import { z } from "zod"
 
-const UpdateReservation = z.object({
-  id: z.number(),
+const User = z.object({
   name: z.string(),
+  email: z.string(),
+  phone: z.string(),
+  meal: z.string(),
+})
+
+const UpdateReservation = z.object({
+  id: z.string(),
+  users: z.array(User),
 })
 
 export default resolver.pipe(
   resolver.zod(UpdateReservation),
   resolver.authorize(),
-  async ({ id, ...data }) => {
+  async ({ id, users }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const reservation = await db.reservation.update({ where: { id }, data })
-
-    return reservation
+    return await db.reservation.update({ where: { id }, data: { users } })
   }
 )
