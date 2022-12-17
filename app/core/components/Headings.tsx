8f4976d1d01908d28e50d80071e0a1dfd64dfd4e
Chinedu Ekene Okpala <allstackdev@gmail.com>
2022-12-17 10:42:45 +0100

    update

--- app/core/components/Headings.tsx
+++ app/core/components/Headings.tsx
@@ -18,49 +18,31 @@ const Headings = ({ standout, title }: { standout: string; title: string }) => {
       </Heading>
       <VStack fontSize={{ base: "xs", lg: "md" }} lineHeight={{ base: "2", lg: "9" }}>
         <Text>
-          Get a bottle of chilled water because it may be a long read but I will try to be concise,
-          as this love story we are celebrating has spanned over a decade of friendship, which story
-          needs to be told…hehe.
+          So we crossed paths with each other, when we were seeking admission to the same
+          university, and that was what brought us together. We went for the school pre-science
+          program, we stayed in the same compound and attended the same fellowship and we became
+          very close friends, nothing other than that, though I have fallen in 😍😉. I left the
+          program because I got accepted to come for a screening at the Nigeria Defence Academy.
         </Text>
         <Image src="/separator.png" alt="separator" />
         <Text>
-          So we cross paths with each other, in Nov 2011, though not in person but in one of “2go”
-          social app chat rooms, which was for Nnamdi Azikiwe University students, where we got
-          information about the admission process, we were teens and focus on gaining admission and
-          furthering our education. We both wanted to go to the same university, and that was what
-          brought us together.
+          Fast forward to a year later, I officially asked her out, and yes, she did not accept nor
+          reject the proposal. This is now becoming a long-distance friendship as we haven’t seen
+          each other for over a year, and at this period I was also finalising my admission to a
+          school in Ghana, this is a whole new point - are we going to make it? We haven’t started
+          and we are already getting separated a lot more.
         </Text>
         <Image src="/separator.png" alt="separator" />
         <Text>
-          We kept in touch, till around 2013 when we went for the school pre-science program, this
-          was when we met for the first time, we lived in the same compound - well orchestrated by
-          `muwa`, and we became very close friends nothing other than that, though I have fallen in
-          love *wink*.
-        </Text>
-        <Image src="/separator.png" alt="separator" />
-        <Text>
-          This program will last for 6months, but I had another plan which was to join the Nigeria
-          Defence Academy, I had to leave and defer the whole program because I had to go for the
-          screening - another period of not seeing each other, which lasted over a year.
-        </Text>
-        <Image src="/separator.png" alt="separator" />
-        <Text>
-          In Nov 2014 I popped the question to officially ask her out, and yes, she did not accept
-          nor reject the proposal. Now know that this is becoming a long-distance friendship as we
-          haven’t seen each other for over a year, and at this period I was finalising my admission
-          to a school in Ghana, this is a whole new point - are we going to make it? I haven’t
-          started and we are already getting separated a lot more.
-        </Text>
-        <Image src="/separator.png" alt="separator" />
-        <Text>
-          But in Dec 2015 we officially started dating and we gave ourselves a chance to work toward
-          it. It wasn’t easy, but we overcame all issues and challenges life has thrown at us. We
-          understood our difference and similarities, We have grown closer since then.
+          Fast forward to a year later, I got a yes and gave ourselves a chance to work toward it.
+          It wasn’t easy, but we overcame all issues and challenges life has thrown at us. We
+          understood our differences and similarities, We have grown closer with ourselves and to
+          God who is our foundation.
         </Text>
         <Image src="/separator.png" alt="separator" />
         <Text>
           Fast track to 2022 I ask the love of my life, if she would want to spend the rest of her
-          days with me, and it was a said YES!!!
+          days with me, and it was a YES!!!
         </Text>
       </VStack>
     </Stack>
